b1218b9d8cbedfabc16bdf98c29bb93079b361fc
Sean Rasmussen <mr.sean.r@gmail.com>
2017-03-12 21:23:37 +1000

    Added validation method in the addressController.

--- app/Http/Controllers/addressController.php
+++ app/Http/Controllers/addressController.php
@@ -4,9 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\addressModel;
+use Log;
 
 class addressController extends Controller
 {
+
+
+    private function validate_input($input)
+    {
+      // sert the required fields
+      $required_fields = array('address_line_1','city','state','zip');
+      // default the return value to true
+      $return_value = true;
+      // setup an array for the input paramter key names
+      $input_keys = array();
+      // iterate through the input data and put the key names into a string array
+      foreach ($input as $key => $value) {
+        //error_log('key: '.$key.' value: '.$value);
+        array_push($input_keys, $key);
+      }
+      // iteragte through the required fields, check if the input keys are in the required fields
+      foreach ($required_fields as $req_field) {
+        error_log('field: '.$req_field);
+        if(!in_array($req_field,$input_keys)) {
+          $return_value=false;
+          //error_log('not found: '.$req_field);
+        }
+      }
+      return $return_value;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -16,7 +42,8 @@ class addressController extends Controller
     {
       // return all addresses
       $addresses = addressModel::all();
-      return response()->json(['status'=>'success','data'=>[$addresses]]);    }
+      return response()->json(['status'=>'success','data'=>[$addresses]]);
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -26,6 +53,13 @@ class addressController extends Controller
      */
     public function store(Request $request)
     {
+      error_log('---------');
+      $input = $request->all();
+      $valid = $this->validate_input($input);
+      if (!$valid){
+        return response()->json(['status'=>'error','message'=>'Not all required fields were supplied.'],422);
+      }
+
       // create the new address using the supplied parameters
       try {
         // TODO check for blank/null parameters
@@ -59,6 +93,8 @@ class addressController extends Controller
       try {
         $address = addressModel::findOrFail($id);
         return response()->json(['status'=>'success','data'=>[$address]]);
+      } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
+        return response()->json(['status'=>'error','message'=>'No resource found.'],404);
       } catch (\Exception $e) {
         return response()->json(['status'=>'error','message'=>$e->getMessage(),'error_code'=>$e->getCode()]);
       }    }
@@ -84,6 +120,8 @@ class addressController extends Controller
         $address->notes=$request->notes;
         $address->save();
         return response()->json(['status'=>'success','data'=>[$address]]);
+      } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
+        return response()->json(['status'=>'error','message'=>'No resource found.'],404);
       } catch (\Illuminate\Database\QueryException $exception) {
         // use php errorInfo class to get the exception
         $errorInfo = $exception->errorInfo;
@@ -104,6 +142,8 @@ class addressController extends Controller
         $address = addressModel::findOrFail($id);
         $address->delete();
         return response()->json(['status'=>'success','message'=>$address->address_line_1.','.$address->city.' was removed successfully.']);
+      } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
+        return response()->json(['status'=>'error','message'=>'No resource found to delete.'],404);
       } catch (\Exception $e) {
         return response()->json(['status'=>'error','message'=>$e->getMessage(),'error_code'=>$e->getCode()]);
       }    }
